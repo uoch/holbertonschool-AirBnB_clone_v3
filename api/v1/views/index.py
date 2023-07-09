ee8f29a4fc6c7f9ccdc67739783fc07e36659aeb
uoch <chaouch.thameurtc61@gmail.com>
2023-07-09 20:13:04 +0100

    fist_status

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,3 +12,13 @@ from flask import Flask
 @app_views.route('/status', strict_slashes=False)
 def api_status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    return jsonify(amenities=storage.count(amenity.Amenity),
+                   cities=storage.count(city.City),
+                   places=storage.count(place.Place),
+                   reviews=storage.count(review.Review),
+                   states=storage.count(state.State),
+                   users=storage.count(user.User))
