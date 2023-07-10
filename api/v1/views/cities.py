b412ca1d9f07e3ecf2d8603f3532d910c9afff35
uoch <chaouch.thameurtc61@gmail.com>
2023-07-10 14:06:11 +0100

    cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""
+mport app_views from api.v1.views
+create a route /status on the object
+"""
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
